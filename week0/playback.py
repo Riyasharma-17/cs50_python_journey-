5b39016ecd889b59e1bb4c2d5ec0bc0c9031ee9d
Riya Sharma <riyasharmaptn1717@gmail.com>
2026-05-14 21:01:22 +0530

    Done playback.py solution for Week 0

---
+++ week0/playback.py
@@ -0,0 +1,20 @@
+# Playback Speed — Python Project
+
+# Some people speak super fast 🎧
+# This program slows the sentence down by replacing every space with ...
+
+# Task
+
+# Create a Python file called playback.py that:
+
+# Takes input from the user
+# Replaces every space with ...
+# Prints the updated sentence
+
+# Example
+# Input:  I love Python
+# Output: I...love...Python
+
+skill = input("Which skill you like? ")
+
+print(skill.replace(" ", "..."))
