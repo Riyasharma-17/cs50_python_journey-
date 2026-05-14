5930420354c264241d23da457ecf344ea5faad28
Riya Sharma <riyasharmaptn1717@gmail.com>
2026-05-14 20:36:33 +0530

    Added Week 0 problem set solutions

--- week0/indoor.py
+++ week0/indoor.py
@@ -1,3 +1,10 @@
+# Indoor Voice is a simple Python program that converts loud uppercase text into lowercase, creating a calm “indoor voice” while keeping spaces and punctuation unchanged 🌙
+
+# Example:
+# Input: HELLO, WORLD!
+# Output: hello, world!
+
+
 sports = input("Whats your favourite sports? ")
 
 sports = sports.lower()
