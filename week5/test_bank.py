4034bb979877f5f085cabf6edeb20a61b9a678d8
Riya Sharma <riyasharmaptn1717@gmail.com>
2026-05-26 17:42:19 +0530

    Solved test_bank.py for bank.py solution for Week 5

---
+++ week5/test_bank.py
@@ -0,0 +1,19 @@
+# test_bank.py
+
+from bank import value
+
+
+def test_hello():
+    assert value("hello") == 0
+
+
+def test_h():
+    assert value("hey") == 20
+
+
+def test_other():
+    assert value("good morning") == 100
+
+
+def test_uppercase():
+    assert value("HELLO") == 0
